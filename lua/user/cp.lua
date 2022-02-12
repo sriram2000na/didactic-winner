fbcfd33fda4f505d26c623b3597bd6429d573fa3
sriram <sriraam2000na@gmail.com>
2022-02-13 03:01:26 +0530

    toggleterm enabled

--- lua/user/cp.lua
+++ lua/user/cp.lua
@@ -1,89 +1,63 @@
-function base(term, torun, input, normal)
-  vim.cmd('lcd %:p:h')
-  local file = vim.fn.expand('%')
-  vim.cmd('FloatermNew --name=cpterm --wintype=split --position=botright --height=0.4 --silent')
-  if input ~= nil then input = string.format(' %s', input) else input = '' end
-  local runs = string.format('%s %s', torun, file) .. input .. ' && exit'
-  print(runs, file)
-
-  local tosend = {
-    'source ~/Desktop/pgrm/cp/compile.sh',
-    runs,
-  }
-  for i=1,#tosend do
-    vim.cmd(string.format('FloatermSend %s', tosend[i]))
-  end
-  if term then
-    vim.cmd('FloatermToggle')
-  end
-  if normal then
-    vim.cmd('stopinsert')
-  end
+local Terminal = require("toggleterm.terminal").Terminal
+function wrapper(cpcommand)
+	vim.cmd("w")
+	cpcommand = cpcommand .. vim.fn.expand("%")
+	local term = Terminal:new({
+		direction = "horizontal",
+		size = 10,
+		-- close_on
+		on_open = function(term)
+			vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+			local tosend = {
+				"source ~/Desktop/pgrm/cp/compile.sh",
+				cpcommand,
+			}
+			-- print(vim.inspect(term))
+			for i = 1, #tosend do
+				vim.cmd(string.format('%sTermExec cmd="%s" go_back=0', term.id, tosend[i]))
+			end
+		end,
+	})
+	return term
 end
-
-function takeinput(succ, fail)
-  -- <CR> is considered success => succ will be called 
-  -- <Esc> is considered failure => fail will be called 
-  vim.cmd('lcd %:p:h')
-  local buf = vim.api.nvim_create_buf(false, true)
-  local ui = vim.api.nvim_list_uis()[1]
-  local width, height = 80, 30
-  local opts = {
-    relative = 'editor',
-    width = 80,
-    height = 30,
-    col = (ui.width / 2) - (width / 2),
-    row = (ui.height / 2) - (height / 2),
-  }
-  vim.api.nvim_open_win(buf, true, opts)
-  vim.cmd('silent !rm in')
-  vim.cmd('silent !touch in')
-  vim.cmd('edit in')
-
-
-  local onsucc = ':wq<CR>'
-  if succ ~= nil then
-    onsucc = onsucc .. string.format(':lua %s()<CR>', succ)
-  end
-
-  local onfail = ':q!<CR>'
-  if fail ~= nil then
-    onfail = onfail .. string.format(':lua %s()<CR>', fail)
-  end
-
-  vim.api.nvim_buf_set_keymap(buf, 'n', '<CR>', onsucc, { noremap = true })
-  vim.api.nvim_buf_set_keymap(buf, 'n', '<Esc>', onfail, { noremap = true })
-end
-
 function prepare()
-  base(false, 'prep')
+	command = "prep "
+	wrapper(command):toggle()
 end
 
 function compile()
-  base(true, 'comp')
+	command = "comp "
+	wrapper(command):toggle()
 end
 
 function compileRun()
-  vim.cmd('w')
-  base(true, 'compr')
+	command = "compr "
+	wrapper(command):toggle()
 end
 
 function compileSubmit()
-  base(true, 'comps')
+	command = "comps "
+	wrapper(command):toggle()
 end
 
 function compileSubmitRun()
-  base(true, 'compsr')
+	command = "compsr "
+	wrapper(command):toggle()
 end
 
 function onlyRun()
-  base(true, 'runc')
+	command = "runc "
+	wrapper(command):toggle()
 end
 
-function runcin()
-  base(true, 'runcin', 'in', true)
-end
-
-function full()
-  takeinput('runcin')
-end
+-- function runcin()
+-- 	file = vim.fn.expand("%")
+-- 	command = "runcin " .. file
+-- 	wrapper(command):toggle()
+-- end
+--
+-- function full()
+-- 	file = vim.fn.expand("%")
+-- 	command = "runcin " .. file
+-- 	wrapper(command):toggle()
+-- end
